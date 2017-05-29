8cd5c09ddff8855860895a28292f4fe87ec41b93
crazyyy <crazyyy@gmail.com>
2017-05-30 00:42:25 +0300

    working on

--- wordpress/wp-content/themes/wp-europark/footer.php
+++ wordpress/wp-content/themes/wp-europark/footer.php
@@ -1,19 +1,74 @@
-    </div><!-- /.inner -->
-  </section><!-- /section -->
-
-</div><!-- /wrapper -->
-
-<footer role="contentinfo">
-  <div class="inner">
-
-    <p class="copyright">
-      &copy; <?php echo date("Y"); ?> Собственность <?php bloginfo('name'); ?>.
-    </p><!-- /copyright -->
-
-  </div><!-- /.inner -->
-</footer><!-- /footer -->
-
-    <?php wp_footer(); ?>
-
+    <!--  Footer. Class fixed for fixed footer  -->
+    <footer class="fixed full-width">
+      <div class="container">
+        <div class="row no-margin">
+          <div class="col-sm-4 col-md-4 padding-leftright-null">
+            <h6 class="heading white margin-bottom-extrasmall">EUROPARK</h6>
+            <p class="footer-p">Хозяйство расположено в краю голубых озер ― на севере Витебской области ― и граничит с территорией России. Угодья нашего охотничье-туристического комплекса занимают площадь в 18 тысяч гектар. Здесь лесной массив перемежается с живописными полями, болотистыми участками, поросшими камышом, и водной гладью озер и рек.</p>
+          </div>
+          <div class="col-sm-4 col-md-4 padding-leftright-null">
+            <h6 class="heading white margin-bottom-extrasmall">Наши контакты</h6>
+            <ul class="info">
+              <li>Телефон: <a href=""> +375 (29) 297-61-21</a></li>
+              <li>Почта: <a href="">europark.by@gmail.com</a></li>
+              <li>Время работы: <span class="white">9:00 - 18:00</span><br></li>
+              <li><a href="">Республика Беларусь, Витебская область, Россонский район, д. Демех, ул.Ольховая 11<br></a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 padding-leftright-null">
+            <h6 class="heading white margin-bottom-extrasmall">Подписка на новости</h6>
+            <p class="grey-light">Нижнедунайская равнина, несмотря на то, что в воскресенье некоторые станции метро закрыты, поднимает холодный действующий вулкан Катмаи.</p>
+            <div id="newsletter-form" class="padding-onlytop-xs">
+              <form class="search-form">
+                <div class="form-input">
+                  <input type="text" placeholder="Укажите ваш e-mail">
+                  <span class="form-button">
+                    <button type="button">Подписаться</button>
+                  </span>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="copy">
+          <div class="row no-margin">
+            <div class="col-md-8 padding-leftright-null">
+              &copy; 2011-2017 Охотничье хозяйство EUROPARK
+            </div>
+            <div class="col-md-4 padding-leftright-null">
+              <ul class="social">
+                <li><a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                <li><a href=""><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                <li><a href=""><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!--  END Footer. Class fixed for fixed footer  -->
+  </div>
+  <!--  Main Wrap  -->
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <?php wp_footer(); ?>
+  <!-- All js library -->
+  <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=false"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.flexslider-min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.fullPage.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/owl.carousel.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/isotope.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.magnific-popup.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.scrollTo.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/smooth.scroll.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.appear.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.countTo.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.scrolly.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/plugins-scroll.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/imagesloaded.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/pace.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
 </body>
 </html>
--- wordpress/wp-content/themes/wp-europark/header.php
+++ wordpress/wp-content/themes/wp-europark/header.php
@@ -5,6 +5,8 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <title>Главная</title>
+  <!-- Bootstrap -->
   <title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' :'; } ?> <?php bloginfo( 'name' ); ?></title>
 
   <link href="http://www.google-analytics.com/" rel="dns-prefetch"><!-- dns prefetch -->
@@ -19,29 +21,97 @@
   <![endif]-->
   <!-- css + javascript -->
   <?php wp_head(); ?>
+
 </head>
 <body <?php body_class(); ?>>
-<!-- wrapper -->
-<div class="wrapper">
-  <header role="banner">
-    <div class="inner">
+  <header>
+    <img class="header-img" src="<?php echo get_template_directory_uri(); ?>/img/header.jpg">
+    <div class="header-layer">
+      <div style="position: absolute; z-index: 0; top: 0px;" class="is_overlay">
+        <video autoplay="autoplay" loop="">
+          <source src="http://europark.by/wp-content/themes/oddel/img/Noch.mp4" type="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;">
 
-      <div class="logo">
-        <?php if ( !is_front_page() && !is_home() ){ ?>
-          <a href="<?php echo home_url(); ?>">
-        <?php } ?>
-            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="<?php wp_title( '' ); ?>" title="<?php wp_title( '' ); ?>" class="logo-img">
-        <?php if ( !is_front_page() && !is_home() ){ ?>
-          </a>
-        <?php } ?>
-      </div><!-- /logo -->
-
-      <nav class="nav" role="navigation">
-        <?php wpeHeadNav(); ?>
-      </nav><!-- /nav -->
-
-    </div><!-- /.inner -->
-  </header><!-- /header -->
-
-  <section role="main">
-    <div class="inner">
+          <img class="header-img" src="<?php echo get_template_directory_uri(); ?>/img/Noch.jpg">
+        </video>
+      </div>
+      <div class="container wrapper">
+        <div class="row valign">
+          <div class="col-md-12 head_right_text">
+            <h1 class="white flex-animation main1">База отдыха</h1>
+            <section class="white flex-animation main2">Диканька</section>
+            <p class="heading white inpage-two main3">Нижнедунайская равнина, несмотря на то, что в воскресенье некоторые станции метро закрыты, поднимает холодный действующий вулкан Катмаи.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!--  loader  -->
+  <div id="myloader">
+    <span class="loader">
+      <div class="inner-loader"></div>
+    </span>
+  </div>
+  <!--  Main Wrap  -->
+  <div id="main-wrap" class="full-width">
+    <!--  Header & Menu  -->
+    <header id="header" class="fixed transparent full-width">
+      <div class="container">
+        <nav class="navbar navbar-default white">
+          <!--  Header Logo  -->
+          <div id="logo">
+            <a class="navbar-brand" href="index.html">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" class="normal" alt="logo">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/logo@2x.png" class="retina" alt="logo">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/logo_white.png" class="normal white-logo" alt="logo">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/logo_white@2x.png" class="retina white-logo" alt="logo">
+            </a>
+          </div>
+          <!--  END Header Logo  -->
+          <!--  Classic menu, responsive menu classic  -->
+          <div id="menu-classic">
+            <div class="menu-holder">
+              <ul>
+                <li class="submenu"><a href="/index.html" class="active-item">Главная</a></li>
+                <li class="submenu"> <a href="/hunting.html">Охота</a> </li>
+                <li class="submenu"> <a href="/fishing.html">Рыбалка</a> </li>
+                <li class="submenu"> <a href="/about-the-base.html">База отдыха</a> </li>
+                <li class="submenu"> <a href="/about-us.html">О нас</a> </li>
+                <li class="submenu"> <a href="/gallery-filters.html">Галерея</a> </li>
+                <li class="submenu"> <a href="/price.html">Прайс</a> </li>
+                <li class="submenu"> <a href="/contact-1.html">Контакты</a> </li>
+                <!-- Search Icon -->
+              </ul>
+            </div>
+          </div>
+          <!--  END Classic menu, responsive menu classic  -->
+          <!--  Button for Responsive Menu Classic  -->
+          <div id="menu-responsive-classic">
+            <div class="menu-button">
+              <span class="bar bar-1"></span>
+              <span class="bar bar-2"></span>
+              <span class="bar bar-3"></span>
+            </div>
+          </div>
+          <!--  END Button for Responsive Menu Classic  -->
+          <!--  Search Box  -->
+          <div id="search-box" class="full-width">
+            <form role="search" id="search-form" class="black big">
+              <div class="form-input">
+                <input class="form-field black big" type="search" placeholder="Search...">
+                <span class="form-button big">
+                  <button type="button">
+                    <i class="icon ion-ios-search"></i>
+                  </button>
+                </span>
+              </div>
+            </form>
+            <button class="close-search-box">
+              <i class="icon ion-ios-close-empty"></i>
+            </button>
+          </div>
+          <!--  END Search Box  -->
+        </nav>
+      </div>
+    </header>
+    <!--  END Header & Menu  -->
+    <!--  Page Content, class footer-fixed if footer is fixed  -->
