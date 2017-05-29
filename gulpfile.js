251c02029c90ea51a9b01340740b804ff8e7e287
crazyyy <crazyyy@gmail.com>
2017-05-29 19:48:39 +0300

    scss okok

--- gulpfile.js
+++ gulpfile.js
@@ -180,7 +180,7 @@ gulp.task('postcss', function() {
     autoprefixer({
       browsers: ['ie >= 8', 'ie_mob >= 10', 'ff >= 20', 'chrome >= 24', 'safari >= 5', 'opera >= 12', 'ios >= 7', 'android >= 2.3', '> 1%', 'last 4 versions', 'bb >= 10']
     }),
-    mqpacker,
+    // mqpacker,
     csswring,
     cssnext()
   ];
