9784cd5f4962bff4999b5e722d042ed0b398600e
crazyyy <crazyyy@gmail.com>
2017-05-30 23:47:23 +0300

    relises

---
+++ wordpress/wp-content/themes/wp-europark/include-calltoaction.php
@@ -0,0 +1,9 @@
+<!-- Call to Action -->
+<div class="row text margin-leftright-null color-background">
+  <div class="col-md-12 text-center">
+    <h4 class="big white moby">Остались вопросы?</h4>
+    <h4 class="big margin-bottom-small white moby1">Свяжитесь с нами <a href="" class="btn-pro simple white moby2">+375 (29) 506-47-07</a></h4>
+    <a href="<?php echo home_url(); ?>/kontakty.htm" target="_blank" class="btn-alt small white margin-null active shadow moby3">Контакты</a>
+  </div>
+</div>
+<!-- END Call to Action -->
--- wordpress/wp-content/themes/wp-europark/page-fishing.php
+++ wordpress/wp-content/themes/wp-europark/page-fishing.php
@@ -186,15 +186,7 @@
       <!--  END Share Btn  -->
       <!-- Similar Trips -->
       <!-- END Similar Trips -->
-      <!-- Call to Action -->
-      <div class="row text margin-leftright-null color-background">
-        <div class="col-md-12 text-center">
-          <h4 class="big white moby">Остались вопросы?</h4>
-          <h4 class="big margin-bottom-small white moby1">Свяжитесь с нами <a href="" class="btn-pro simple white moby2">+375 (29) 506-47-07</a></h4>
-          <a href="#" target="_blank" class="btn-alt small white margin-null active shadow moby3">Контакты</a>
-        </div>
-      </div>
-      <!-- END Call to Action -->
+      <?php get_template_part('include-calltoaction'); ?>
     </div>
   </div>
   <?php endwhile; endif; ?>
